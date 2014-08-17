967f361be6695d9693e2cd1ba2effbd033d4c161
Catherine Tran <tranc489@gmail.com>
2014-08-16 20:42:16 -0700

    set up main menu with main choices.

--- cook_book_ui.rb
+++ cook_book_ui.rb
@@ -1,3 +1,27 @@
 require './lib/meal'
 require './lib/ingredient'
 
+def main_menu
+  loop do
+    system 'clear'
+    puts "***COOK BOOK***"
+    puts "Press 'm' to create a new meal in the Cook Book."
+    puts "Press 'l' to list all meals"
+    puts "Press 'x' to exit."
+    main_choice = gets.chomp
+    if main_choice == 'm'
+      create_meal
+    elsif main_choice == 'l'
+      list_meals
+    elsif main_choice == 'x'
+      puts "***THANK YOU, COME AGAIN LATER***\n\n"
+      sleep(1)
+      exit
+    else
+      puts "Not a valid entry.  Please try again."
+      sleep(1.5)
+    end
+  end
+end
+
+main_menu
