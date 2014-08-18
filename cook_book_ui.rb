6372191755e9b211d8e5e09d6a430d7ac8a33515
Catherine Tran <tranc489@gmail.com>
2014-08-17 17:10:08 -0700

    make create_meal method in UI for user to be able to input a meal and save.

--- cook_book_ui.rb
+++ cook_book_ui.rb
@@ -1,9 +1,9 @@
 require './lib/meal'
 require './lib/ingredient'
 
+
 def main_menu
   loop do
-    system 'clear'
     puts "*** COOK BOOK ***"
     puts "Press 'm' to create a new meal in the Cook Book."
     puts "Press 'l' to list all meals"
@@ -15,14 +15,27 @@ def main_menu
       list_meals
     elsif main_choice == 'x'
       puts "***THANK YOU, COME AGAIN LATER***\n\n"
-      sleep(1)
+      sleep(1.5)
+      system 'clear'
       exit
     else
       puts "Not a valid entry.  Please try again."
       sleep(1.5)
+      system 'clear'
     end
   end
 end
+
+def create_meal
+  puts "*** NEW MEAL ***"
+  puts "Please enter the name of your meal:"
+  meal_input = gets.chomp
+  new_meal = Meal.new(meal_input)
+  Meal.new(new_meal).save
+  puts "*" + "#{new_meal.name}" + "*" " has been saved in your recipe book.\n\n"
+  sleep(1)
+end
+
 
 
 main_menu
